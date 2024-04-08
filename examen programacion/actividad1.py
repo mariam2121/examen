f78fc3bf32087283db6e5414bf98c76d26c23510
mariam2121 <112910300+mariam2121@users.noreply.github.com>
2024-04-08 15:46:03 -0300

    Add files via upload

---
+++ examen programacion/actividad1.py
@@ -0,0 +1,25 @@
+print("bienbenido usuario")
+
+print("agregar la cantidad de filas que decee tener en su matriz, tienen que ser mayor o igual a 1")
+filas = input()
+print("agregar la cantidad de columnas decee tener, tiene que ser mayor o igual a 1")
+columnas = input()
+
+resultado =  int(filas) * int(columnas)
+
+print(f"la cantidad de numeros que tiene que proporcionar son la multiplicacion de filas y columnas son: {resultado}" )  
+
+
+
+for num in range(resultado):
+    print(f"escribir {resultado} numeros")
+    num = input()
+
+print("los numeros fueron agregados a una matriz")
+
+
+
+
+
+    
+
